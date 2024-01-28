7295cb40f05fac1ed93ed0862901a8fa891217de
ngenethlis <ngenethlis@gmail.com>
2024-01-28 22:57:55 +0000

    update to bloom filter class

--- code/bloomfilter/bloom_filter.py
+++ code/bloomfilter/bloom_filter.py
@@ -1,27 +1,21 @@
-import ABC
 import hashlib
 import mmh3
 from bitarray import bitarray
 
 
-
-
-
-class BloomFilter(ABC):
-    def __init__(self,size,k=3):
+class BloomFilter:
+    def __init__(self, size, k=3):
         hashF = [
             lambda x: mmh3.hash(x, 0),
             lambda x: hashlib.sha256(str(x).encode()).digest().__hash__()
         ]
         pass
 
-    def add(self,element):
+    def add(self, element):
         pass
 
-    def __contains__(self,element):
+    def __contains__(self, element):
         pass
 
     def __len__(self):
         pass
-
-
